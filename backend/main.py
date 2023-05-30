6abea3e8c43bcc78ec5eb5504cfbdc551c257c01
onosaito <onosaito0614@gmail.com>
2023-05-30 15:46:24 +0530

    `.env` support in `backend` for windows

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,8 @@
 import uvicorn
+from dotenv import load_dotenv
 
+# Load environment variables from .env file
+load_dotenv()
 
 if __name__ == "__main__":
     uvicorn.run("app.api:app", host="0.0.0.0", port=8000, reload=True)
