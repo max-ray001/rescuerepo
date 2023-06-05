c101094af9773792d309f0d2dcdac1de0f950ddc
onosaito <onosaito0614@gmail.com>
2023-06-04 20:23:05 -0700

    refactor(frontend): added error handling to web-vitals

--- frontend/src/reportWebVitals.js
+++ frontend/src/reportWebVitals.js
@@ -1,12 +1,17 @@
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
+    import('web-vitals')
+      .then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+        getCLS(onPerfEntry);
+        getFID(onPerfEntry);
+        getFCP(onPerfEntry);
+        getLCP(onPerfEntry);
+        getTTFB(onPerfEntry);
+      })
+      .catch(err => {
+        // Handle or report error here
+        console.error('Failed to load web-vitals', err);
+      });
   }
 };
   
